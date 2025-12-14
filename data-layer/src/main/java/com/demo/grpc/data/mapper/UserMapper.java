a1340c707a8d5ae2c2bbcde8aaf2231b81536afb
takahiro-kaji-1122 <takahiro.kaji@seattleconsulting.co.jp>
2025-12-15 03:20:51 +0900

    mapperの記述修正

--- data-layer/src/main/java/com/demo/grpc/data/mapper/UserMapper.java
+++ data-layer/src/main/java/com/demo/grpc/data/mapper/UserMapper.java
@@ -8,22 +8,24 @@ import java.util.List;
 @Mapper
 public interface UserMapper {
 
-    @Results(id = "userResultMap", value = {
-            @Result(property = "userId", column = "user_id", id = true),
-            @Result(property = "name", column = "name"),
-            @Result(property = "email", column = "email")
-    })
-    @Select("SELECT user_id, name, email FROM users WHERE user_id = #{userId}")
-    UserEntity findById(@Param("userId") String userId);
+        @Select("""
+                        SELECT user_id, name, email
+                        FROM users
+                        WHERE user_id = #{userId}
+                        """)
+        UserEntity findById(@Param("userId") String userId);
 
-    @Results({
-            @Result(property = "userId", column = "user_id", id = true),
-            @Result(property = "name", column = "name"),
-            @Result(property = "email", column = "email")
-    })
-    @Select("SELECT user_id, name, email FROM users ORDER BY user_id LIMIT #{limit} OFFSET #{offset}")
-    List<UserEntity> findAll(@Param("limit") int limit, @Param("offset") int offset);
+        @Select("""
+                        SELECT user_id, name, email
+                        FROM users
+                        ORDER BY user_id
+                        LIMIT #{limit} OFFSET #{offset}
+                        """)
+        List<UserEntity> findAll(@Param("limit") int limit, @Param("offset") int offset);
 
-    @Select("SELECT COUNT(*) FROM users")
-    int count();
+        @Select("""
+                        SELECT COUNT(*)
+                        FROM users
+                        """)
+        int count();
 }
